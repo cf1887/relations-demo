2b124802f9b8c0c161f243a5f40e211334b1a323
cf1887 <firefistholly@gmail.com>
2025-09-23 11:45:44 +0200

    initial commit: setup for relations-demo

---
+++ src/main/java/dev/cf1887/relations_demo/RelationsDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.cf1887.relations_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cf1887/relations_demo/RelationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cf1887.relations_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
